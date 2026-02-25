fd421e70c8c3dafa6daabea985c55ceb9ae724d5
kiet <tuankietcoder2211@gmail.com>
2026-02-25 19:51:07 +0700

    header component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router-dom";
+import { Header } from "../components/common";
 
 export default function MainLayout() {
   return (
-    <div className= "h-screen w-screen ">
-      <Outlet/>
+    <div className="min-h-screen w-screen bg-dark-950">
+      <Header />
+      <main>
+        <Outlet/>
+      </main>
     </div>
   )
 }
